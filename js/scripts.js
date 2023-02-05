d26322ac9778f31e8437ee5bf9c4db00bdba1f3e
Fouad <f.ahsayni@pm.me>
2023-02-05 13:04:37 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,20 @@
+alert('Hello world');
+
+let favoriteFood = 'Couscous';
+document.write(favoriteFood);
+
 // Creating array to hold data on Pokémon
 let pokemonList = [];
 
 // Adding several Pokémon and their data to the array
 pokemonList = [
-  { name: "Bulbasaur", height: 0.7, weight: 6.9, type: ["grass", "poison"] },
-  { name: "Ivysaur", height: 1, weight: 13, type: ["grass", "poison"] },
-  { name: "Venusaur", height: 2, weight: 100, type: ["grass", "poison"] },
-  { name: "Charmander", height: 0.6, weight: 8.5, type: ["fire"] },
-  { name: "Charmeleon", height: 1.1, weight: 19, type: ["fire"] },
-  { name: "Charizard", height: 1.7, weight: 90.5, type: ["fire", "flying"] },
-  { name: "Squirtle", height: 0.5, weight: 9, type: ["water"] },
-  { name: "Wartortle", height: 1, weight: 22.5, type: ["water"] },
-  { name: "Blastoise", height: 1.6, weight: 85.5, type: ["water"] },
+  { name: 'Bulbasaur', height: 0.7, weight: 6.9, type: ['grass', 'poison'] },
+  { name: 'Ivysaur', height: 1, weight: 13, type: ['grass', 'poison'] },
+  { name: 'Venusaur', height: 2, weight: 100, type: ['grass', 'poison'] },
+  { name: 'Charmander', height: 0.6, weight: 8.5, type: ['fire'] },
+  { name: 'Charmeleon', height: 1.1, weight: 19, type: ['fire'] },
+  { name: 'Charizard', height: 1.7, weight: 90.5, type: ['fire', 'flying'] },
+  { name: 'Squirtle', height: 0.5, weight: 9, type: ['water'] },
+  { name: 'Wartortle', height: 1, weight: 22.5, type: ['water'] },
+  { name: 'Blastoise', height: 1.6, weight: 85.5, type: ['water'] },
 ]
