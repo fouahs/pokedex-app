f354196de978748ecbbd09e6140ea8401efc26c4
Fouad <f.ahsayni@pm.me>
2023-02-05 16:26:10 +0100

    Add for loop to create list of Pokémon on the page

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,18 @@ pokemonList = [
   { name: 'Wartortle', height: 1, weight: 22.5, type: ['water'] },
   { name: 'Blastoise', height: 1.6, weight: 85.5, type: ['water'] },
 ]
+
+// Creating a for loop iterating over all Pokémon and writing them on the website
+for (let i = 0; i < pokemonList.length; i++) {
+
+  // Adding a conditional so that the biggest Pokémon gets highlighted
+  if (pokemonList[i].height > 1.8) {
+    document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow, that's big!`);
+    document.write('<br>');
+  } else {
+    document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height})`);
+    document.write('<br>');
+  }
+
+}
+
